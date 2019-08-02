51c0d07c930384a4bd0e6e69ea8bc3b4a68024f6
Black <gmocore@gmail.com>
2019-08-02 14:40:11 -0600

    fixed search string. clicking button adds 1 image to page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,36 @@
 const topics = [
   "goodfellas",
   "dogs",
-  "mind blown",
+  "mind+blown",
   "timelapse",
   "work",
-  "american psycho",
-  "fight club",
+  "american+psycho",
+  "fight+club",
   "space",
-  "assassins creed odyssey",
+  "assassins+creed+odyssey",
   "gta v"
 ];
+
+let chosenTopic;
+
+const searchString = `https://api.giphy.com/v1/gifs/search?api_key=0q2XqKx6Ynz8T8KJYTzu1FMObhUHEv0X&limit=10&rating=PG&lang=en&q=`;
 
 topics.forEach((item, index) => {
   const button = `<button id="${index}">${item}</button>`;
   $(".buttons").append(button);
   $(`#${index}`).click(function() {
-    console.log(topics[index], "clicked");
+    chosenTopic = topics[index];
+    console.log(searchString + chosenTopic);
+    searchString + chosenTopic;
+    $.ajax({
+      url: searchString + chosenTopic,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response.data);
+      $(".image-container").append(
+        `<img src="${response.data[3].images.downsized.url}"></img>`
+      );
+      //   $(".image-container").append(`<p>oh hey</p>`);
+    });
   });
 });
