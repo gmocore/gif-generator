527e080bca596f2732e177ba0d23f92de8831d29
gerritt <gmocore@gmail.com>
2019-08-03 09:28:08 -0600

    click displays multiple gifs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,41 @@
 const topics = [
   "goodfellas",
   "dogs",
-  "mind+blown",
+  "mind blown",
   "timelapse",
   "work",
-  "american+psycho",
-  "fight+club",
+  "american psycho",
+  "fight club",
   "space",
-  "assassins+creed+odyssey",
+  "assassins creed odyssey",
   "gta v"
 ];
 
 let chosenTopic;
 
 const searchString = `https://api.giphy.com/v1/gifs/search?api_key=0q2XqKx6Ynz8T8KJYTzu1FMObhUHEv0X&limit=10&rating=PG&lang=en&q=`;
-
+let image;
 topics.forEach((item, index) => {
   const button = `<button id="${index}">${item}</button>`;
   $(".buttons").append(button);
   $(`#${index}`).click(function() {
     chosenTopic = topics[index];
-    console.log(searchString + chosenTopic);
-    searchString + chosenTopic;
     $.ajax({
       url: searchString + chosenTopic,
       method: "GET"
     }).then(function(response) {
-      console.log(response.data);
-      $(".image-container").append(
-        `<img src="${response.data[3].images.downsized.url}"></img>`
-      );
-      //   $(".image-container").append(`<p>oh hey</p>`);
+      response.data.forEach(element => {
+        console.log(element)
+        image = element
+        $(".image-container").append(
+          `<img src="${element.images.downsized_still.url}"></img>`);
+      
+      
+      })
+      $('img').mouseover(e => {
+        console.log(e);
+        $(e.target).attr('src', image.images.downsized.url)
+      })
     });
   });
 });
