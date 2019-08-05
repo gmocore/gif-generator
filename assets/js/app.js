271b8b1a15de0fbe5a6a13c962777a8513a01226
Black <gmocore@gmail.com>
2019-08-05 15:45:11 -0600

    put everything in render function to create buttons dynamically

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,34 +11,43 @@ const topics = [
   "gta v"
 ];
 
-let chosenTopic;
+// let chosenTopic;
 
 const searchString = `https://api.giphy.com/v1/gifs/search?api_key=0q2XqKx6Ynz8T8KJYTzu1FMObhUHEv0X&limit=10&rating=PG&lang=en&q=`;
-let image;
-topics.forEach((item, index) => {
-  const button = `<button id="${index}">${item}</button>`;
-  $(".buttons").append(button);
-  $(`#${index}`).click(function() {
-    chosenTopic = topics[index];
-    $.ajax({
-      url: searchString + chosenTopic,
-      method: "GET"
-    }).then(function(response) {
-      $(".image-container").empty();
-      response.data.forEach(element => {
-        console.log(element);
-        $(".image-container").append(
-          `<img src="${element.images.downsized_still.url}" data-gif="${
-            element.images.downsized.url
-          }" data-still="${element.images.downsized_still.url}"></img>`
-        );
-      });
-      $("img").mouseenter(function(e) {
-        $(e.target).attr("src", $(e.target).attr("data-gif"));
-      });
-      $("img").mouseleave(function(e) {
-        $(e.target).attr("src", $(e.target).attr("data-still"));
+
+$("#add-button").click(function() {
+  console.log("clicked");
+  topics.push($("#topic-input").val());
+  console.log(topics);
+  makeStuff();
+});
+function makeStuff() {
+  $(".buttons").empty();
+  topics.forEach((item, index) => {
+    const button = `<button id="${index}">${item}</button>`;
+    $(".buttons").append(button);
+    $(`#${index}`).click(function() {
+      let chosenTopic = topics[index];
+      $.ajax({
+        url: searchString + chosenTopic,
+        method: "GET"
+      }).then(function(response) {
+        $(".image-container").empty();
+        response.data.forEach(element => {
+          console.log(element);
+          $(".image-container").append(
+            `<img src="${element.images.downsized_still.url}" data-gif="${
+              element.images.downsized.url
+            }" data-still="${element.images.downsized_still.url}"></img>`
+          );
+        });
+        $("img").mouseenter(function(e) {
+          $(e.target).attr("src", $(e.target).attr("data-gif"));
+        });
+        $("img").mouseleave(function(e) {
+          $(e.target).attr("src", $(e.target).attr("data-still"));
+        });
       });
     });
   });
-});
+}
